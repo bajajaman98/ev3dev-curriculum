1c746b0e50ef2014786c7f724d20a31ed2486c63
bajaja <bajaja@rose-hulman.edu>
2018-01-31 09:45:13 -0500

    added stuff to m2

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -33,6 +33,7 @@ class Snatch3r(object):
         if speed_deg_per_second != 0:
             self.left_motor.run_to_rel_pos(position_sp=rotations_in_degrees, speed_sp=speed_deg_per_second)
             self.right_motor.run_to_rel_pos(position_sp=rotations_in_degrees, speed_sp=speed_deg_per_second)
+            print("hi")
             self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
             ev3.Sound.beep().wait()
 
