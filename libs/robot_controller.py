72828c99ee7061814328773cc134ab34856ed7b8
nordqunn <nordqunn@rose-hulman.edu>
2018-02-01 08:16:22 -0500

    moved the robot_controller.py 'goodbye' statement to the third module

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -48,5 +48,4 @@ class Snatch3r(object):
             self.left_motor.run_to_rel_pos(position_sp=-degrees_spin_wheel, speed_sp=turn_speed_sp)
             self.right_motor.run_to_rel_pos(position_sp=degrees_spin_wheel, speed_sp=turn_speed_sp)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-        print('is something happening')
-        ev3.Sound.speak("What the fuck").wait()
+        
