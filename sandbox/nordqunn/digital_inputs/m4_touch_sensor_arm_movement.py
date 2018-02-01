1bd0b6b9b96f37d09044956744339b327920cd88
nordqunn <nordqunn@rose-hulman.edu>
2018-02-01 09:38:29 -0500

    if not removed

--- sandbox/nordqunn/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/nordqunn/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -78,7 +78,7 @@ def arm_calibration(arm_motor, touch_sensor):
     arm_motor.stop(stop_action=ev3.Motor.STOP_ACTION_BRAKE)
     ev3.Sound.beep().wait()
 
-    arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
+    arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range, speed_sp=900)
     arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
     ev3.Sound.beep().wait()
 
@@ -136,7 +136,7 @@ def arm_down(arm_motor):
     Type hints:
       :type arm_motor: ev3.MediumMotor
     """
-    # TODO: 5. Implement the arm up movement by fixing the code below
+    # DONE: 5. Implement the arm up movement by fixing the code below
     # Move the arm to the absolute position_sp of 0 at max speed.
     # Wait until the move completes
     # Make a beep sound
@@ -147,7 +147,7 @@ def arm_down(arm_motor):
     arm_motor.wait_while(ev3.Motor.STATE_RUNNING)  # Blocks until the motor finishes running
     ev3.Sound.beep().wait()
 
-    # TODO: 6. After you fix the bugs in the three arm movement commands demo your code to a TA or instructor.
+    # done: 6. After you fix the bugs in the three arm movement commands demo your code to a TA or instructor.
     #
     # Observations you should make, the TouchSensor is easy to use, but the motor commands are still a little bit
     #   tricky.  It is neat that the same motor API works for both the wheels and the arm.
