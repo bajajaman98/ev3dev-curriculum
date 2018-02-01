465740b5a036a6067daa54baab1d446ca0acb250
bajaja <bajaja@rose-hulman.edu>
2018-01-31 21:22:37 -0500

    added stuff to m2

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -40,14 +40,14 @@ class Snatch3r(object):
         ev3.Sound.speak("Goodbye").wait()
 
     def turn_degrees(self,degrees_to_turn,turn_speed_sp):
-        degrees_per_turning_degree = 10
+        degrees_per_turning_degree = 4.7
         degrees_spin_wheel = degrees_to_turn * degrees_per_turning_degree
         if turn_speed_sp < 0:
-            self.right_motor.run_to_rel_pos(position_sp=degrees_spin_wheel,speed_sp=turn_speed_sp)
-            self.left_motor.run_to_rel_pos(position_sp=-degrees_spin_wheel, speed_sp=turn_speed_sp)
-        elif turn_speed_sp > 0:
+            self.right_motor.run_to_rel_pos(position_sp=-degrees_spin_wheel,speed_sp=turn_speed_sp)
             self.left_motor.run_to_rel_pos(position_sp=degrees_spin_wheel, speed_sp=turn_speed_sp)
-            self.right_motor.run_to_rel_pos(position_sp=-degrees_spin_wheel, speed_sp=turn_speed_sp)
+        elif turn_speed_sp > 0:
+            self.left_motor.run_to_rel_pos(position_sp=-degrees_spin_wheel, speed_sp=turn_speed_sp)
+            self.right_motor.run_to_rel_pos(position_sp=degrees_spin_wheel, speed_sp=turn_speed_sp)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         print('is something happening')
         ev3.Sound.speak("What the fuck").wait()
