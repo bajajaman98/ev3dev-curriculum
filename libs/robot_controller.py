7387041f6bd77d822f16386dee0124eae198aabe
nordqunn <nordqunn@rose-hulman.edu>
2018-02-01 08:56:08 -0500

    update

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -34,7 +34,7 @@ class Snatch3r(object):
             self.left_motor.run_to_rel_pos(position_sp=rotations_in_degrees, speed_sp=speed_deg_per_second)
             self.right_motor.run_to_rel_pos(position_sp=rotations_in_degrees, speed_sp=speed_deg_per_second)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-        
+
 
 
 
