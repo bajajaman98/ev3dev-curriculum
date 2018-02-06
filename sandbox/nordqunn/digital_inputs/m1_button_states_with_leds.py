554be95695613e9de81e8672fbab459c016e7f1e
nordqunn <nordqunn@rose-hulman.edu>
2018-02-05 19:57:43 -0500

    done

--- sandbox/nordqunn/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/nordqunn/digital_inputs/m1_button_states_with_leds.py
@@ -8,7 +8,7 @@ Much like we did in bajaja of the motors unit, later we will show you different 
 Authors: David Fisher and Nathaniel Neil Nate Nordquist.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
+# DONE: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
 #   You will exit the program by pressing the back button on the EV3 brick (button just below the screen).
 #   The back button is already implemented to exit the program (as you can see in the code below).
 
@@ -112,13 +112,12 @@ def main():
         #   Press Down - Both LEDs are Red (the cycle repeats)
         #   Press Back - Both LEDs turn Green, the robot says Goodbye and the program exits
 
-        # TODO: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+        # DONE: 6. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
         #
         # Observation you should make, working with buttons as 'states' is functional but usually 'events' work better.
         # Also observe that we don't use the Enter button.  Enter can cause issues since your program is running at the
         #   same time as the Brickman operating system.  Both are receiving the button events.  That can be changed, but
         #   it's too much trouble to do here.  So instead we just don't use the Enter button.
-
         if btn.backspace:
             break
         time.sleep(0.01)  # Best practice to have a short delay to avoid working too hard between loop iterations.
