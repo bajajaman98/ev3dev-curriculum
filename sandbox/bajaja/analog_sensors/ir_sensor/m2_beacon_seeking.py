4bc6c3f5f136a8dd21b87dca02ec8048ca111848
bajaja <bajaja@rose-hulman.edu>
2018-02-12 08:30:12 -0500

    an actual message for once

--- sandbox/bajaja/analog_sensors/ir_sensor/m2_beacon_seeking.py
+++ sandbox/bajaja/analog_sensors/ir_sensor/m2_beacon_seeking.py
@@ -29,11 +29,11 @@ def main():
     robot = robo.Snatch3r()
     try:
         while True:
-            seek_beacon(robot)
-
-            # TODO: 5. Save the result of the seek_beacon function (a bool), then use that value to only say "Found the
+            if seek_beacon(robot):
+                ev3.Sound.speak("Found the beacon")
+                print("Found the beacon")
+            # DONE: 5. Save the result of the seek_beacon function (a bool), then use that value to only say "Found the
             # beacon" if the return value is True.  (i.e. don't say "Found the beacon" if the attempts was cancelled.)
-            ev3.Sound.speak("Found the beacon")
 
             command = input("Hit enter to seek the beacon again or enter q to quit: ")
             if command == "q":
@@ -72,7 +72,7 @@ def seek_beacon(robot):
             print("IR Remote not found. Distance is -128")
             robot.stop()
         else:
-            # TODO: 4. Implement the following strategy to find the beacon.
+            # DONE: 4. Implement the following strategy to find the beacon.
             # If the absolute value of the current_heading is less than 2, you are on the right heading.
             #     If the current_distance is 0 return from this function, you have found the beacon!  return True
             #     If the current_distance is greater than 0 drive straight forward (forward_speed, forward_speed)
@@ -95,11 +95,14 @@ def seek_beacon(robot):
                 robot.drive_inches(1,300)
                 if current_distance == 0:
                     return True
-            if math.fabs(current_distance) > 2 and math.fabs(current_distance) < 10:
+            if math.fabs(current_heading) > 2 and math.fabs(current_heading) < 10:
+                print("Adjusting heading: ", current_heading)
                 if current_heading > 0:
                     robot.turn_degrees(1,300)
                 else:
                     robot.turn_degrees(-1,300)
+            if math.fabs(current_heading) > 10:
+                print("Heading is too far off to fix")
                 # You add more!
 
 
